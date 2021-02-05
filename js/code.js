9d52cb1400cbbd612f54a039b257637b204f0743
Jay Doshi <55336105+CodeJ-dev@users.noreply.github.com>
2021-02-05 14:01:41 -0500

    Update code.js

--- js/code.js
+++ js/code.js
@@ -1,97 +1,9 @@
-var urlBase = 'http://COP4331-5.com/LAMPAPI';
+var urlBase = 'http://numpal.xyz/API';
 var extension = 'php';
 
 var userId = 0;
 var firstName = "";
 var lastName = "";
-
-function doLogin()
-{
-	userId = 0;
-	firstName = "";
-	lastName = "";
-	
-	var login = document.getElementById("loginName").value;
-	var password = document.getElementById("loginPassword").value;
-//	var hash = md5( password );
-	
-	document.getElementById("loginResult").innerHTML = "";
-
-//	var jsonPayload = '{"login" : "' + login + '", "password" : "' + hash + '"}';
-	var jsonPayload = '{"login" : "' + login + '", "password" : "' + password + '"}';
-	var url = urlBase + '/Login.' + extension;
-
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", url, false);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try
-	{
-		xhr.send(jsonPayload);
-		
-		var jsonObject = JSON.parse( xhr.responseText );
-		
-		userId = jsonObject.id;
-		
-		if( userId < 1 )
-		{
-			document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
-			return;
-		}
-		
-		firstName = jsonObject.firstName;
-		lastName = jsonObject.lastName;
-
-		saveCookie();
-	
-		window.location.href = "color.html";
-	}
-	catch(err)
-	{
-		document.getElementById("loginResult").innerHTML = err.message;
-	}
-
-}
-
-function saveCookie()
-{
-	var minutes = 20;
-	var date = new Date();
-	date.setTime(date.getTime()+(minutes*60*1000));	
-	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
-}
-
-function readCookie()
-{
-	userId = -1;
-	var data = document.cookie;
-	var splits = data.split(",");
-	for(var i = 0; i < splits.length; i++) 
-	{
-		var thisOne = splits[i].trim();
-		var tokens = thisOne.split("=");
-		if( tokens[0] == "firstName" )
-		{
-			firstName = tokens[1];
-		}
-		else if( tokens[0] == "lastName" )
-		{
-			lastName = tokens[1];
-		}
-		else if( tokens[0] == "userId" )
-		{
-			userId = parseInt( tokens[1].trim() );
-		}
-	}
-	
-	if( userId < 0 )
-	{
-		window.location.href = "index.html";
-	}
-	else
-	{
-		document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
-	}
-}
 
 function doLogout()
 {
@@ -102,57 +14,97 @@ function doLogout()
 	window.location.href = "index.html";
 }
 
-function addColor()
+function addContact()
 {
-	var newColor = document.getElementById("colorText").value;
-	document.getElementById("colorAddResult").innerHTML = "";
-	
-	var jsonPayload = '{"color" : "' + newColor + '", "userId" : ' + userId + '}';
-	var url = urlBase + '/AddColor.' + extension;
-	
+	var firstName = document.getElementById("contactFirstName").value;
+	var lastName = document.getElementById("contactLastName").value;
+	var phone = document.getElementById("contactPhoneNumber").value;
+	var email = document.getElementById("contactEmail").value;
+	var street = document.getElementById("contactStreet").value;
+	var city = document.getElementById("contactCity").value;
+	var state = document.getElementById("contactState").value;
+	var zipcode = document.getElementById("contactZipcode").value;
+	var country = document.getElementById("contactCountry").value;
+	document.getElementById("contactAddResult").innerHTML = "";
+
+	var jsonPayload = '{"FirstName" : ' + firstName + ', "LastName" : ' + lastName + ', "Phone" : ' + phone +
+			', "Email" : ' + email + ', "Street" : ' + street + ', "City" : ' + city +
+			', "State" : ' + state + ', "Zipcode" : ' + zipcode + ', "Country" : ' + country + '}';
+
+	alert(jsonPayload);
+
+	var url = urlBase + '/AddContact.' + extension;
+
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 	try
 	{
-		xhr.onreadystatechange = function() 
+		xhr.onreadystatechange = function()
 		{
-			if (this.readyState == 4 && this.status == 200) 
+			if (this.readyState == 4 && this.status == 200)
 			{
-				document.getElementById("colorAddResult").innerHTML = "Color has been added";
+				document.getElementById("contactAddResult").innerHTML = "Contact has been added";
 			}
 		};
 		xhr.send(jsonPayload);
 	}
 	catch(err)
 	{
-		document.getElementById("colorAddResult").innerHTML = err.message;
+		document.getElementById("contactAddResult").innerHTML = err.message;
 	}
-	
+
 }
 
-function searchColor()
+function searchContact()
+{
+	// var temp = "A B <br />\r\nC D<br />\r\nE F<br />\r\nG H<br />\r\nI J<br />\r\n";
+	document.getElementById("searchResult").style.display = "block";
+	// alert(temp);
+	// document.getElementsByTagName("p")[0].innerHTML = "";
+	// document.getElementsByTagName("p")[0].innerHTML = temp;
+
+	// Get just and <a> elements
+	/*
+	var anchors = document.querySelectorAll("a");
+
+	// Set up a click event listener for each one
+	for(var i = 0; i < anchors.length; i++){
+		anchors[i].addEventListener("click", function(evt){
+			return (evt.target.id);
+		});
+	}
+	*/
+}
+
+function editContact(evt)
+{
+	alert(evt.target.id);
+}
+
+/*
+function searchContact()
 {
 	var srch = document.getElementById("searchText").value;
 	document.getElementById("colorSearchResult").innerHTML = "";
-	
+
 	var colorList = "";
-	
+
 	var jsonPayload = '{"search" : "' + srch + '","userId" : ' + userId + '}';
 	var url = urlBase + '/SearchColors.' + extension;
-	
+
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 	try
 	{
-		xhr.onreadystatechange = function() 
+		xhr.onreadystatechange = function()
 		{
-			if (this.readyState == 4 && this.status == 200) 
+			if (this.readyState == 4 && this.status == 200)
 			{
 				document.getElementById("colorSearchResult").innerHTML = "Color(s) has been retrieved";
 				var jsonObject = JSON.parse( xhr.responseText );
-				
+
 				for( var i=0; i<jsonObject.results.length; i++ )
 				{
 					colorList += jsonObject.results[i];
@@ -161,7 +113,7 @@ function searchColor()
 						colorList += "<br />\r\n";
 					}
 				}
-				
+
 				document.getElementsByTagName("p")[0].innerHTML = colorList;
 			}
 		};
@@ -171,5 +123,6 @@ function searchColor()
 	{
 		document.getElementById("colorSearchResult").innerHTML = err.message;
 	}
-	
+
 }
+*/
