57a207e243fb174d6789c9bd0b939607bda40b1e
kdevlinjr <77754777+kdevlinjr@users.noreply.github.com>
2021-02-05 14:04:07 -0500

    added login and register functions

--- js/code.js
+++ js/code.js
@@ -5,6 +5,79 @@ var userId = 0;
 var firstName = "";
 var lastName = "";
 
+function doLogin() {
+	userId = 0;
+	firstName = "";
+	lastName = "";
+
+	var login = document.getElementById("loginName").value;
+	var password = document.getElementById("loginPassword").value;
+	//	var hash = md5( password );
+
+	document.getElementById("loginResult").innerHTML = "";
+
+	//	var jsonPayload = '{"login" : "' + login + '", "password" : "' + hash + '"}';
+	var jsonPayload = '{"login" : "' + login + '", "password" : "' + password + '"}';
+	var url = urlBase + '/Login.' + extension;
+
+	alert(jsonPayload);
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, false);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try {
+		xhr.send(jsonPayload);
+
+		var jsonObject = JSON.parse(xhr.responseText);
+
+		userId = jsonObject.id;
+
+		if (userId < 1) {
+			document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+			return;
+		}
+		firstName = jsonObject.firstName;
+		lastName = jsonObject.lastName;
+   
+
+		saveCookie();
+    
+		window.location.href = "contacts.html";
+    alert(window.location.pathname);
+	}
+	catch (err) {
+		document.getElementById("loginResult").innerHTML = err.message;
+	}
+
+}
+
+function doRegister() {
+	var firstName = document.getElementById("firstName").value;
+	var lastName = document.getElementById("lastName").value;
+	var login = document.getElementById("registerName").value;
+	var password = document.getElementById("registerPassword").value;
+
+	document.getElementById("registerResult").innerHTML = "";
+
+	var jsonPayload = '{"firstName" : "' + firstName + '", "lastName" : "' + lastName + '", "login" : "' + login + '", "password" : "' + password + '"}';
+	var url = urlBase + '/CreateAccount.' + extension;
+
+	alert(jsonPayload);
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, false);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try {
+		xhr.send(jsonPayload);
+
+		window.location.href = "index.html";
+	}
+	catch (err) {
+		document.getElementById("registerResult").innerHTML = err.message;
+	}
+
+}
+
 function doLogout()
 {
 	userId = 0;
